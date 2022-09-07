e1ad72511033f3fbaa318bef251c538d30aa3373
camilohm1 <cahernandez3510@misena.edu.co>
2022-09-07 12:02:54 -0500

    ejerciciosjavascrip

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,7 +1,13 @@
 addEventListener ("DOMContentLoaded", ()=>{
 //7. Escribir un programa que calcule el volumen de una esfera: Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
 
+let Radio=3
+let pi=3.14
+let v=4/3
+let Vesfera=float
+let (v*pi*Radio**3)
 
+console.log("VOLUMEN DE LA ESFERA",Vesfera)
 
 
 
